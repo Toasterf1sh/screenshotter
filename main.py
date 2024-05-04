da0ec093926296f7312c9f0fa5e657300465e04a
Alyssa <106944368+Toasterf1sh@users.noreply.github.com>
2024-05-04 21:56:43 +1000

    Update main.py

--- main.py
+++ main.py
@@ -6,21 +6,25 @@ from mss import mss, tools
 mouse = Controller()
 
 print("moving mouse")
+
+# This should be the coordinates of the page turn button
 mouse.position = (1406, 832)
 
-#for campion 
+#Screen capture sizes for different textbook readers below: 
+
+#campion 
     # mouse.position = (1083.30859375, 166.30859375)
     # monitor = {"top": 202, "left": 468, "width": 481, "height": 694}
 
-#for pearson
+#pearson
     # mouse.position = (1406, 832)
     # monitor = {"top": 186, "left": 519, "width": 496, "height": 698}
 
-#for iitomo (pearson)
+#iitomo (pearson)
     # mouse.position = (1406, 832)
     # monitor = {"top": 186, "left": 488, "width": 557, "height": 699}
 
-#for signpost (pearson)
+#signpost (pearson)
     # mouse.position = (1406, 832)
     # monitor = {"top": 186, "left": 501, "width": 531, "height": 698}
 
@@ -28,8 +32,10 @@ print("starting 'camera'")
 counter = 0
 with mss() as sct:
     while True:
-        # The screen part to capture
+        
+        # Replace this with the coordinates of the top left and bottom right corners of the page
         monitor = {"top": 186, "left": 501, "width": 531, "height": 698}
+        
         sct_img = sct.grab(monitor)
         tools.to_png(sct_img.rgb, sct_img.size, output=f"image-{counter}.png")
 
