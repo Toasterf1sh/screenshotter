7435b3c131155438c3f2142457c28bb436152b47
Toasterf1sh <106944368+Toasterf1sh@users.noreply.github.com>
2024-05-17 21:30:14 +1000

    new commit

--- main.py
+++ main.py
@@ -7,41 +7,30 @@ mouse = Controller()
 
 print("moving mouse")
 
-# This should be the coordinates of the page turn button
-mouse.position = (1406, 832)
+# Replace x, y with coordinates of the page turn button
+mouse.position = (x, y)
 
-#Screen capture sizes for different textbook readers below: 
-
-#campion 
-    # mouse.position = (1083.30859375, 166.30859375)
-    # monitor = {"top": 202, "left": 468, "width": 481, "height": 694}
-
-#pearson
-    # mouse.position = (1406, 832)
-    # monitor = {"top": 186, "left": 519, "width": 496, "height": 698}
-
-#iitomo (pearson)
-    # mouse.position = (1406, 832)
-    # monitor = {"top": 186, "left": 488, "width": 557, "height": 699}
-
-#signpost (pearson)
-    # mouse.position = (1406, 832)
-    # monitor = {"top": 186, "left": 501, "width": 531, "height": 698}
 
 print("starting 'camera'")
 counter = 0
 with mss() as sct:
     while True:
-        
-        # Replace this with the coordinates of the top left and bottom right corners of the page
-        monitor = {"top": 186, "left": 501, "width": 531, "height": 698}
-        
+
+        # Replace x,y,a,b with respective coordinates of the dimensions 
+        # of the size of screenshot to be taken: 
+        # top - top left corner y component
+        # left - top left corner x component
+        # width - bottom right corner x component
+        # height - bottom right corner y component
+
+        monitor = {"top": y, "left": x, "width": a, "height": b}
         sct_img = sct.grab(monitor)
         tools.to_png(sct_img.rgb, sct_img.size, output=f"image-{counter}.png")
 
         print("sleeping")
         time.sleep(2)
 
+        # For screenshotting the full page, replace "sct.grab(monitor)" with: 
         # sct.shot(output=f"snip{counter}.png")
         mouse.click(Button.left)
 
